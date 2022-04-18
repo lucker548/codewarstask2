7d2384a310dadde6d468a5c9f41902cff0e774a7
Nurzhan Abayev <nurzhanabayev@MacBook-Air-Nurzhan.local>
2022-04-18 10:33:07 +0200

    regular task

---
+++ codewarstask.js
@@ -0,0 +1,16 @@
+function rowSumOddNumbers(n) {
+    return n * n * n;
+}
+
+
+/*
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+
+
+calculate the sum of the row 
+*/
+
